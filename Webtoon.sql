ab034e4bf7676d56687d2f0eac7c19768e161f8f
thjvjpxz <2151160532@e.tlu.edu.vn>
2025-03-25 23:55:23 +0700

    Remove .cursorignore and update Webtoon.sql

--- Webtoon.sql
+++ Webtoon.sql
@@ -1,16 +1,17 @@
 CREATE TABLE `users` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
 	`username` VARCHAR(255) NOT NULL UNIQUE,
 	`email` VARCHAR(255) NOT NULL UNIQUE,
 	`password` VARCHAR(255) NOT NULL,
 	`img_url` VARCHAR(255),
 	`vip` BIT DEFAULT 0,
 	`active` BIT DEFAULT 0,
-	`role_id` VARCHAR(255) NOT NULL,
+	`role_id` VARCHAR(36) NOT NULL,
 	`balance` DOUBLE NOT NULL,
 	`last_topup` TIMESTAMP NOT NULL,
-	`level_id` VARCHAR(255) NOT NULL,
+	`current_level` TINYINT NOT NULL,
 	`current_exp` DOUBLE,
+	`level_type_id` VARCHAR(36),
 	`created_at` TIMESTAMP,
 	`updated_at` TIMESTAMP,
 	PRIMARY KEY(`id`)
@@ -18,7 +19,7 @@ CREATE TABLE `users` (
 
 
 CREATE TABLE `comics` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
 	`name` VARCHAR(255) NOT NULL,
 	`slug` VARCHAR(255) NOT NULL UNIQUE,
 	`origin_name` VARCHAR(255),
@@ -35,23 +36,26 @@ CREATE TABLE `comics` (
 
 
 CREATE TABLE `categories` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
 	`name` VARCHAR(255) NOT NULL,
 	`slug` VARCHAR(255) NOT NULL UNIQUE,
+	`description` TEXT(65535),
+	`created_at` TIMESTAMP,
+	`updated_at` TIMESTAMP,
 	PRIMARY KEY(`id`)
 );
 
 
 CREATE TABLE `comic_categories` (
-	`comic_id` VARCHAR(255) NOT NULL,
-	`category_id` VARCHAR(255) NOT NULL,
+	`comic_id` VARCHAR(36) NOT NULL,
+	`category_id` VARCHAR(36) NOT NULL,
 	PRIMARY KEY(`comic_id`, `category_id`)
 );
 
 
 CREATE TABLE `chapters` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`comic_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`comic_id` VARCHAR(36) NOT NULL,
 	`title` VARCHAR(255) NOT NULL,
 	`status` ENUM('fee', 'free', 'vip') NOT NULL DEFAULT 'fee',
 	`number` INTEGER NOT NULL,
@@ -64,8 +68,8 @@ CREATE TABLE `chapters` (
 CREATE INDEX `chapters_index_0`
 ON `chapters` (`comic_id`);
 CREATE TABLE `detail_chapters` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`chapter_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`chapter_id` VARCHAR(36) NOT NULL,
 	`img_url` VARCHAR(255) NOT NULL,
 	`order_number` SMALLINT NOT NULL,
 	PRIMARY KEY(`id`, `chapter_id`)
@@ -73,9 +77,9 @@ CREATE TABLE `detail_chapters` (
 
 
 CREATE TABLE `comments` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`user_id` VARCHAR(255) NOT NULL,
-	`chapter_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`user_id` VARCHAR(36) NOT NULL,
+	`chapter_id` VARCHAR(36) NOT NULL,
 	`content` TEXT(65535) NOT NULL,
 	`status` ENUM('active', 'inactive', 'deleted') NOT NULL DEFAULT 'active',
 	`created_at` TIMESTAMP,
@@ -85,40 +89,40 @@ CREATE TABLE `comments` (
 
 
 CREATE TABLE `favorites` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`user_id` VARCHAR(255) NOT NULL,
-	`comic_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`user_id` VARCHAR(36) NOT NULL,
+	`comic_id` VARCHAR(36) NOT NULL,
 	`created_at` TIMESTAMP,
 	PRIMARY KEY(`id`)
 );
 
 
 CREATE TABLE `roles` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
 	`name` VARCHAR(255) NOT NULL,
 	PRIMARY KEY(`id`)
 );
 
 
 CREATE TABLE `permissions` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`role_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`role_id` VARCHAR(36) NOT NULL,
 	`permission` VARCHAR(255),
 	PRIMARY KEY(`id`)
 );
 
 
 CREATE TABLE `publishers` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`user_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`user_id` VARCHAR(36) NOT NULL,
 	`status` ENUM('pending', 'approved', 'rejected'),
 	PRIMARY KEY(`id`)
 );
 
 
 CREATE TABLE `topups` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`user_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`user_id` VARCHAR(36) NOT NULL,
 	`amount` DOUBLE NOT NULL,
 	`status` ENUM('success', 'pending', 'failed'),
 	`created_at` TIMESTAMP NOT NULL,
@@ -127,9 +131,9 @@ CREATE TABLE `topups` (
 
 
 CREATE TABLE `user_purchased_chapters` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
-	`user_id` VARCHAR(255) NOT NULL,
-	`chapter_id` VARCHAR(255) NOT NULL,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`user_id` VARCHAR(36) NOT NULL,
+	`chapter_id` VARCHAR(36) NOT NULL,
 	`amount` DOUBLE,
 	`created_at` TIMESTAMP,
 	PRIMARY KEY(`id`)
@@ -137,9 +141,22 @@ CREATE TABLE `user_purchased_chapters` (
 
 
 CREATE TABLE `levels` (
-	`id` VARCHAR(255) NOT NULL UNIQUE,
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`level_type_id` VARCHAR(36),
+	`level` TINYINT NOT NULL,
 	`name` VARCHAR(255) NOT NULL,
+	`color_name` VARCHAR(7),
 	`exp_required` DOUBLE NOT NULL,
+	`url_gif` VARCHAR(255),
+	`created_at` TIMESTAMP,
+	`updated_at` TIMESTAMP,
+	PRIMARY KEY(`id`)
+);
+
+
+CREATE TABLE `level_types` (
+	`id` VARCHAR(36) NOT NULL UNIQUE,
+	`name` VARCHAR(255) NOT NULL,
 	`created_at` TIMESTAMP,
 	`updated_at` TIMESTAMP,
 	PRIMARY KEY(`id`)
@@ -182,12 +199,15 @@ ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `topups`
 ADD FOREIGN KEY(`user_id`) REFERENCES `users`(`id`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE `users`
-ADD FOREIGN KEY(`level_id`) REFERENCES `levels`(`id`)
-ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `user_purchased_chapters`
 ADD FOREIGN KEY(`user_id`) REFERENCES `users`(`id`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `user_purchased_chapters`
 ADD FOREIGN KEY(`chapter_id`) REFERENCES `chapters`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `levels`
+ADD FOREIGN KEY(`level_type_id`) REFERENCES `level_types`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `users`
+ADD FOREIGN KEY(`level_type_id`) REFERENCES `level_types`(`id`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
