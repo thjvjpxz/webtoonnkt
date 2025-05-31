41e5cee3d94d9111cedba8bf75e917dd41cf4022
thjvjpxz <2151160532@e.tlu.edu.vn>
2025-05-31 08:00:14 +0700

    feat: enhance footer component styling and improve accessibility

--- frontend-comic/src/components/Footer.tsx
+++ frontend-comic/src/components/Footer.tsx
@@ -4,22 +4,22 @@ import { FaFacebook, FaGithub, FaTwitter } from "react-icons/fa";
 const socialLinks = [
   {
     href: "https://www.facebook.com/thi.17.8",
-    icon: <FaFacebook className="w-6 h-6" />,
+    icon: <FaFacebook className="w-6 h-6 text-[var(--primary)] hover:text-[var(--success)] transition-all duration-200" />,
   },
   {
     href: "https://github.com/thjvjpxz",
-    icon: <FaGithub className="w-6 h-6" />,
+    icon: <FaGithub className="w-6 h-6 text-[var(--primary)] hover:text-[var(--success)] transition-all duration-200" />,
   }
 ];
 
 export default function Footer() {
   return (
-    <footer className="bg-[var(--background)] text-[var(--foreground)] border-t border-[var(--border)]">
+    <footer className="bg-[var(--background)]  border-t border-[var(--border)]">
       <div className="flex flex-col items-center justify-center py-6">
-        <p className="text-sm text-center">
+        <p className="text-sm text-center text-[var(--foreground)]">
           &copy; 2025 Nguyễn Kim Thi
         </p>
-        <div className="mt-3 flex justify-center items-center gap-2">
+        <div className="mt-3 flex justify-center items-center gap-2 ">
           {
             socialLinks.map((link) => (
               <Link
